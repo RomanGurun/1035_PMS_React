69fffbf81072108c9991dcc7090088e597f69cbf
Roman Gurung <romangurung569@gmail.com>
2024-04-28 00:34:38 +0545

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className="navbar">
+  <a href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#products">Products</a>
+  <a href="#contact">Contact</a>
+</div>
+
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Home.css'
+import {useState} from 'react'
+import  Navbar from '../../components/Navbar/Navbar';
+import {useEffect} from 'react'
+import axios from 'axios';
+const Home = () => {
+const [products,setProducts]=useState([]);
+
+const fetchProducts=async()=>{
+
+const response = await axios.get("https://662d26660547cdcde9e012c2.mockapi.io/products");
+setProducts(response.data);
+
+console.log(response);
+}
+
+useEffect(()=>{
+fetchProducts();
+
+
+},[])
+console.log(products);
+
+
+
+return (
+<>
+<Navbar/>
+
+
+{
+  products.map((product)=>{
+return(
+    <div key={product.id} className="card">
+      {console.log(product.id)}
+    <img src={product.productImage} alt="Product Image"/>
+    {console.log(product.productImage)}
+    <div className="content">
+      <div className="productName">{product.productName}</div>
+      <div className="productDescription">{product.productDescription}</div>
+      <p>{product.productMaterial}</p>
+    </div>
+  </div>
+)
+
+})
+}
+
+
+  </>
+
+  )
+}
+
+export default Home
